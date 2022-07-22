5a3800b51485c4186c0a175ec8b114537ed1b4d9
AJAY VARDHAN <64157453+ajayvardhan77@users.noreply.github.com>
2022-07-22 17:41:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 // for player1
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;// 1- 6
 var randomNum1Image = "dice"+randomNumber1+".png" // dice1.png - dice6.png
-var randomNum1ImageSrc = "images\"+randomNum1Image
+var randomNum1ImageSrc = "images/"+randomNum1Image
 var img1 = document.querySelectorAll("img")[0];
 img1.setAttribute("src",randomNum1ImageSrc)
 
 //for player2
 var randomNum2 = Math.floor(Math.random() * 6) + 1;// 1 - 6
 var randNum2Img = "dice"+randomNum2+".png"
-var randNum2ImgSrc = "images\"+randNum2Img
+var randNum2ImgSrc = "images/"+randNum2Img
 var img2 = document.querySelectorAll("img")[1];
 img2.setAttribute("src",randNum2ImgSrc)
 
